c02319023d9409ebaf4e68f0b127a6b4436339fc
usho64k <usuallybreadspace@gmail.com>
2019-10-24 00:58:19 +0900

    Any tasks move test

--- testcode/pearl.c
+++ testcode/pearl.c
@@ -8,28 +8,43 @@
 
 
 
-
-
 #define THIS_NULL		(void *)(0)
 
 static int itIsTrue = 0;
 static int itIsTrue2 = 0;
+static int itIsTrue3 = 1;
 
-void printffff(void);
+//タスク1用
 static void countInit(void);
 static void countLoop(void);
 static void countLoop2(void);
+static void countLoop3(void);
+//タスク2用
+static void countInit2(void);
+static void countLoop2_3(void);
 
 T_TSK task1 = {THIS_NULL,0,128};
+T_TSK task2 = {THIS_NULL,0,128};
 
 T_JUDGE judge1 = {E_VALIABLE,&itIsTrue,&itIsTrue};
 T_JUDGE judge2 = {E_VALIABLE,&itIsTrue2,&itIsTrue2};
+T_JUDGE judge3 = {E_VALIABLE,&itIsTrue3,&itIsTrue3};
 
 T_FUNKS func1 = {&judge1,countInit};
 T_FUNKS func2 = {&judge1,countLoop};
 T_FUNKS func3 = {&judge2,countLoop2};
+T_FUNKS func4 = {&judge3,countLoop3};
+
+T_FUNKS func_task2[3] = 
+{
+	{&judge1,countInit2},
+	{&judge1,countLoop2_3},
+	{&judge2,countLoop2_3},
+};
+
 
 static int count;
+static int count2;
 
 void main(void)
 {
@@ -39,13 +54,23 @@ void main(void)
 		//ここにタスクを登録していく
 		ercd = createTask(0,&task1);
 		while(ercd < 0){printf("error at create");}
+		ercd = createTask(1,&task2);
+		while(ercd < 0){printf("error at create");}
 
 		ercd = setTaskFunc(0,&func1);
 		while(ercd < 0){printf("error at setTaskFunc1");}
 		ercd = setTaskFunc(0,&func2);
 		while(ercd < 0){printf("error at setTaskFunc2");}
 
+		ercd = setTaskFunc(1,&func_task2[0]);
+		while(ercd < 0){printf("error at setTaskFunc2");}
+		ercd = setTaskFunc(1,&func_task2[1]);
+		while(ercd < 0){printf("error at setTaskFunc2");}
+		
+
 		ercd = startTask(0);
+		while(ercd < 0){printf("error at start");}
+		ercd = startTask(1);
 		while(ercd < 0){printf("error at start");}
 		
 
@@ -71,8 +96,21 @@ static void countLoop2(void)
 	printf("Output %d\r\n",count);
 }
 
-
-void printffff(void)
+static void countLoop3(void)
 {
-	printf("supersamurai\r\n");
+	count+=3;
+	printf("Output %d\r\n",count);
+}
+
+static void countInit2(void)
+{
+	//タスク2のInitialize
+	count2 = 0;
+	printf("Task 2 Initialized\n");
+}
+
+static void countLoop2_3(void)
+{
+	count2+=3;
+	printf("Output:::: %d\r\n",count2);
 }
